ee146ed5d40505854df36e5afffe40d0ee1eae14
Abhishek Mishra <abhishekmishra@Abhisheks-MacBook-Pro.local>
2023-04-02 23:02:15 +0530

    Demo version with gradio

--- api_gradio.py
+++ api_gradio.py
@@ -5,11 +5,11 @@ from steamship import check_environment, RuntimeEnvironments, Steamship
 from steamship.invocable import post, PackageService
 
 
-class MermaidGenerator(PackageService):
-    @post("generate")
-    def generate(self, input_text: str) -> str:
-        """Generate Diagrams from plain English."""
-        prompt = f"System: Using mermaid.js, create a diagram that shows the entities involved, the transactions between them, and the events that occur during the process of {input_text}. Please provide a high-level overview of the process and highlight any important decision points or outcomes. Reply only with mermaid.js code and brief description or explanation. However, you can think step-by-step. Enclose your answer with __begin__ and __end__ tag. The diagram type should be automatically selected based on the task."
+class LemonToFeature(PackageService):
+    @post("convert")
+    def convert(self, input_text: str) -> str:
+        """Convert criticism or roast into potential feature, test, or use case."""
+        prompt = f"System: Convert the following criticism or roast into potential features, tests, or use cases: {input_text}. Reply with a list of potential features, tests, or use cases. Enclose your answer with __begin__ and __end__ tag."
 
         # Generate text
         task = self.client.use_plugin('gpt-4').generate(text=prompt)
@@ -17,48 +17,25 @@ class MermaidGenerator(PackageService):
         # Wait for completion of the task.
         task.wait()
 
-        # Get the mermaid code from the response
-        mermaid_code_match = re.search(r"```mermaid([\s\S]*)```", task.output.blocks[0].text)
+        # Get the response from the GPT4 plugin
+        response_match = re.search(r"__begin__([\s\S]*)__end__", task.output.blocks[0].text)
 
-        if mermaid_code_match:
+        if response_match:
             # Save response to file
             with open("gpt4_response.txt", "w") as f:
                 f.write(task.output.blocks[0].text)
 
-            # Generate diagram.html file from mermaid code
-            mermaid_code = mermaid_code_match.group(1).strip().strip('`')
-            diagram_html = f"""
-                <html>
-                    <head>
-                        <style>
-                            .mermaid {{
-                                /* Remove animation */
-                                /* mermaidActive 1s ease-in-out infinite */
-                            }}
-                        </style>
-                        <script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
-                        <script>mermaid.initialize({{startOnLoad:true}});</script>
-                    </head>
-                    <body>
-                        <div class="mermaid">
-                            """ + mermaid_code + """
-                        </div>
-                    </body>
-                </html>
-                """
-            with open("diagram.html", "w") as f:
-                f.write(diagram_html)
-
-
-            return diagram_html
+            # Return the potential features, tests, or use cases
+            return response_match.group(1).strip()
         else:
-            return "No diagram found in the response."
+            return "No potential features, tests, or use cases found in the response."
 
-def message(input_msg):
+
+def convert(input_msg):
     with Steamship.temporary_workspace() as client:
-         mermaid_generator = MermaidGenerator(client)
-         return mermaid_generator.generate(input_text=input_msg)
+         lemon_converter = LemonToFeature(client)
+         return lemon_converter.convert(input_text=input_msg)
 
 
-demo = gr.Interface(fn=message, inputs="text", outputs="html")
+demo = gr.Interface(fn=convert, inputs="text", outputs="text")
 demo.launch()
